08267b7f781d5fde06b27cec4159107f90492be0
watemeion <g.yubao@gmail.com>
2015-05-24 21:44:50 +0800

    Initial Commit

---
+++ AppTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppTemplate
+//
+//  Created by Mac on 5/24/15.
+//  Copyright (c) 2015 Studio Of Spicy Hot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppTemplate
+//
+//  Created by Mac on 5/24/15.
+//  Copyright (c) 2015 Studio Of Spicy Hot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
